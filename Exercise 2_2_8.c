a4996fdc09dea4eeecd290259156660ac3619e48
SegeiNikiforov <pclp@inbox.ru>
2020-10-04 19:03:04 +0300

    Create Exercise 2_2_8.c

---
+++ Exercise 2_2_8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define MAX 100
+int A[MAX];
+void SelectionSort(int A[], int n);
+
+int main() {
+    int i, n;
+/* read and store input in A */
+    for (n = 0; n < MAX && scanf("%d", &A[n]) != EOF; n++)
+        ;
+
+    SelectionSort(A,n); /* sort A */
+    printf("\n");
+    for (i = 0; i < n; i++)
+        printf("%d\n", A[i]); /* print A */
+}
+
+void SelectionSort(int *A, int n) {
+    int i, j, small, temp;
+
+    for (i = 0; i < n-1; i++) {
+        small = i;
+        for (j = i+1; j < n; j++)
+            if (*(A + j) < *(A + small))
+                small = j;
+
+        temp = *(A + small);
+        *(A + small) = *(A + i);
+        *(A + i) = temp;
+    }
+}
