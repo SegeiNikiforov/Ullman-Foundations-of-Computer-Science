508b12d2423e47781d361b85c5a0bf21043ab4cf
SegeiNikiforov <pclp@inbox.ru>
2020-10-05 19:58:14 +0300

    Create array_sum_recursion.c

---
+++ array_sum_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+int getSum(int [], int);
+int main()
+{
+    int n;
+    int A[MAX];
+
+    for (n = 0; n < MAX && scanf("%d", &A[n]) != EOF; n++);
+
+    printf("\nThen sum of array: %d\n", getSum(A, n));
+    return 0;
+}
+
+int getSum(int A[], int n) {
+    if (n <= 0) {
+        return 0;
+    }
+
+    return getSum(A, n - 1) + A[n - 1];
+}
