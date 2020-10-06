fa5b10b3ee062bc5f6aab28b63ce67a9b98e5bfe
SegeiNikiforov <pclp@inbox.ru>
2020-10-06 10:59:28 +0300

    Create Exercise 2_2_11.c

---
+++ Exercise 2_2_11.c
@@ -0,0 +1,32 @@
+Exercise 2.2.11: Use the Σ and π notations described at the beginning of this chapter to
+express the following.
+
+a) The sum of the odd integers from 1 to 377
+
+b) The sum of the squares of the even integers from 2 to n (assume that n is even)
+
+c) The product of the powers of 2 from 8 to 2k
+
+a) // *******************************************************************************
+
+377
+
+Σ     f(i)
+
+i%2=1
+
+b) // *******************************************************************************
+
+n
+
+Σ     f(i*i)
+
+i%2=0
+
+c) // *******************************************************************************
+
+2000
+
+π     f(2^i)
+
+i=8
